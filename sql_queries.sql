3d2b35d39ad01d5fc89b08a063ad110e71599ec3
Diallo <mamadouadama609@gmail.com>
2024-07-04 00:16:03 -0500

    with joins

--- sql_queries.sql
+++ sql_queries.sql
@@ -154,6 +154,18 @@ where p.first_name like 's%s'
 and len(p.first_name) >= 6
 ;
 
+/*
+Show patient_id, first_name, last_name from 
+patients whos diagnosis is 'Dementia'.
+Primary diagnosis is stored in the admissions table.
+*/
+
+select  p.patient_id, p.first_name, p.last_name
+from
+patients p
+join admissions a 
+on p.patient_id = a.patient_id
+where a.diagnosis = 'Dementia';
 
 
 
